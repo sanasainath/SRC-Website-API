3f24527538b384c772417dead332ba695d9574b8
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-14 10:08:39 +0530

    Update user-model.js

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         enum: ['user', 'admin'],
-        required: true,
+        required: false,
         default: 'user'
     },
     isVerified: {
