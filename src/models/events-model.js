3f19db3b525160fd8e85f1cbc91384ef06ab642e
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-19 09:14:52 +0530

    Update events-model.js

--- src/models/events-model.js
+++ src/models/events-model.js
@@ -8,7 +8,7 @@ const eventSchema = new Schema({
     fromDate: { type: Date, required: true },
     toDate: { type: Date, required: true },
     startTime : {type : String, required : true},
-    toTime : {type : String, required : true},
+    endTime : {type : String, required : true},
     time:{type : String, required : true},
     prizeDetails : [{position: Number, description : String}],
     description: { type: String, required: true },
