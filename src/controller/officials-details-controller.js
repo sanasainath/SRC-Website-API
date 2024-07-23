7536c5f82000c7a702af0ef29c50cf9d46447b3c
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-23 18:47:21 +0530

    Update officials-details-controller.js

--- src/controller/officials-details-controller.js
+++ src/controller/officials-details-controller.js
@@ -84,7 +84,21 @@ class OfficialController {
 
     updateOfficial=async (req, res)=> {
         try {
-            const official = await this.officialService.updateOfficial(req.params.id, req.body);
+            const officialData = req.body;
+            console.log("Req file", req.file);
+
+            if (req.file) {
+                const filePath = req.file.path;
+                const fileBuffer = fs.readFileSync(filePath);
+                const fileBase64 = fileBuffer.toString('base64');
+                console.log("in controller:", filePath, fileBuffer, fileBase64);
+                // Add the Base64 image to testimonialData
+                officialData.photo = fileBase64;
+
+                // Delete the file after converting to Base64
+                fs.unlinkSync(filePath);
+            }
+            const official = await this.officialService.updateOfficial(req.params.id, officialData);
             res.status(200).json({
                 data:official,
                 success:true,
