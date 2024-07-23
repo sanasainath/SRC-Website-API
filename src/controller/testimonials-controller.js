a9a8592f53cf7eba82694c063cb1d990ad5705d4
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-23 14:45:11 +0530

    Update testimonials-controller.js

--- src/controller/testimonials-controller.js
+++ src/controller/testimonials-controller.js
@@ -62,7 +62,23 @@ class TestimonialController {
 
     async updateTestimonial(req, res) {
         try {
-            const testimonial = await testimonialService.updateTestimonial(req.params.id, req.body);
+            const testimonialData = req.body;
+            console.log("Req file", req.file);
+
+            if (req.file) {
+                const filePath = req.file.path;
+                const fileBuffer = fs.readFileSync(filePath);
+                const fileBase64 = fileBuffer.toString('base64');
+                console.log("in controller:", filePath, fileBuffer, fileBase64);
+                // Add the Base64 image to testimonialData
+                testimonialData.photo = fileBase64;
+
+                // Delete the file after converting to Base64
+                fs.unlinkSync(filePath);
+            }
+
+            const testimonial = await testimonialService.updateTestimonial(req.params.id, testimonialData);
+             console.log("in controller test:", testimonial);
             if (!testimonial) return res.status(404).json({ message: 'Testimonial not found' });
             res.status(200).json(testimonial);
         } catch (error) {
