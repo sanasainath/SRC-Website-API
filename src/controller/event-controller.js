009ad6a04fc338f28d9361b904d581af35739b13
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-24 17:53:45 +0530

    Update event-controller.js

--- src/controller/event-controller.js
+++ src/controller/event-controller.js
@@ -6,14 +6,6 @@ class EventController {
         this.eventService = new EventService();
     }
 
-    // createEvent = async (req, res) => {
-    //     try {
-    //         const event = await this.eventService.createEvent(req.body);
-    //         return res.status(201).json(event);
-    //     } catch (error) {
-    //         return res.status(400).json({ error: error.message });
-    //     }
-    // }
     createEvent = async (req, res) => {
         try {
             const eventData =req.body;
@@ -74,7 +66,37 @@ class EventController {
 
     updateEvent = async (req, res) => {
         try {
-            const event = await this.eventService.updateEvent(req.params.id, req.body);
+            const eventData =req.body;
+            console.log("req body re",req.body);
+            console.log("Req files", req.files);
+            if (req.files && req.files.length > 0) {
+                // Initialize an array to store Base64 encoded images
+                const imagesBase64 = [];
+    
+                // Iterate over each file in req.files
+                for (const file of req.files) {
+                    const filePath = file.path;
+    
+                    // Read file and convert to Base64
+                    try {
+                        const fileBuffer = fs.readFileSync(filePath);
+                        const fileBase64 = fileBuffer.toString('base64');
+    
+                        // Add the Base64 image to the array
+                        imagesBase64.push(fileBase64);
+    
+                        // Delete the file after converting to Base64
+                        fs.unlinkSync(filePath);
+                    } catch (err) {
+                        console.error('Error reading or deleting file:', err);
+                        return res.status(500).json({ message: 'Error processing file' });
+                    }
+                }
+    
+                // Add the array of Base64 images to newsData
+                eventData.images = imagesBase64;
+            }
+            const event = await this.eventService.updateEvent(req.params.id, eventData);
             res.status(200).json(event);
         } catch (error) {
             res.status(404).json({ error: error.message });
