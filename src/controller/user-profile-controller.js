6d44481d6b5b0b37cb144deccad9f0d0f97eb9f0
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-24 20:01:17 +0530

    Update user-profile-controller.js

--- src/controller/user-profile-controller.js
+++ src/controller/user-profile-controller.js
@@ -1,7 +1,8 @@
 const UserProfileService = require('../services/user-profile-service'); // Importing as instance, not a class
 const { StatusCodes } = require('http-status-codes'); 
 const userProfileService=new UserProfileService();
-
+const fs = require("fs");
+const multer = require("multer");
 class UserProfileController {
   async getAllUserProfiles(req, res) {
     try {
@@ -48,7 +49,29 @@ async getUserProfileByEmail(req, res, next) {
   async updateUserProfile(req, res) {
     console.log("in profile controller:",req.params.id,req.body);
     try {
-      const profile = await userProfileService.updateUserProfile(req.params.id, req.body);
+    
+        const profileData = req.body;
+        console.log("Req file", req.file);
+
+        if (req.file) {
+            const filePath = req.file.path;
+
+            // Read file and convert to Base64
+            try {
+                const fileBuffer = fs.readFileSync(filePath);
+                const fileBase64 = fileBuffer.toString('base64');
+
+                // Add the Base64 image to newsData
+                profileData.image = fileBase64;
+
+                // Delete the file after converting to Base64
+                fs.unlinkSync(filePath);
+            } catch (err) {
+                console.error('Error reading or deleting file:', err);
+                return res.status(500).json({ message: 'Error processing file' });
+            }
+      
+      const profile = await userProfileService.updateUserProfile(req.params.id, profileData);
       console.log("in profile controller user:",profile);
       if (!profile) return res.status(404).json({ message: 'UserProfile not found' });
       res.status(200).json(profile);
