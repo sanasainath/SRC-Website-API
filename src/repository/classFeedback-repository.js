e5ef4d262a2baf0226539c0058035d9d34fb45a9
Nagarjuna <99315689+Nagarjuna0033@users.noreply.github.com>
2024-07-27 21:07:38 +0530

    Create classFeedback-repository.js

---
+++ src/repository/classFeedback-repository.js
@@ -0,0 +1,10 @@
+const CrudRepository = require('./crud-repository');
+const ClassFeedback = require('../models/class-feedback-model');
+
+class ClassFeedbackRepository extends CrudRepository {
+    constructor() {
+        super(ClassFeedback);
+    }
+}
+
+module.exports = ClassFeedbackRepository;
